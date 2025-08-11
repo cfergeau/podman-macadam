77fab68e806662b9f9ba4d42434a77e2b6881443
lstocchi <lstocchi@redhat.com>
2025-08-11 10:23:21 +0200

    allow to update exclusiveActive value
    podman does not allow to run multiple VM at the same time, except for
    WSL. However, macadam should allow it with all providers. This patch adds a set func to
    update the exclusiveActive value and allow to run multiple VMs
    simultaneously.
    The default behavior does not change.
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- pkg/machine/qemu/stubber.go
+++ pkg/machine/qemu/stubber.go
@@ -37,6 +37,7 @@ type QEMUStubber struct {
 var (
 	gvProxyWaitBackoff        = 500 * time.Millisecond
 	gvProxyMaxBackoffAttempts = 6
+	exclusiveActive           = true
 )
 
 func (q *QEMUStubber) UserModeNetworkEnabled(*vmconfigs.MachineConfig) bool {
@@ -47,8 +48,12 @@ func (q *QEMUStubber) UseProviderNetworkSetup(_ *vmconfigs.MachineConfig) bool {
 	return false
 }
 
+func (q *QEMUStubber) SetExclusiveActive(exclusive bool) {
+	exclusiveActive = exclusive
+}
+
 func (q *QEMUStubber) RequireExclusiveActive() bool {
-	return true
+	return exclusiveActive
 }
 
 func (q *QEMUStubber) setQEMUCommandLine(mc *vmconfigs.MachineConfig) error {
