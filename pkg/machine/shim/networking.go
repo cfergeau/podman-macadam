lstocchi/podman

extend ssh check to use ip address
it uses the VM IP and default SSH port to check if the VM is running and
connect to it

Signed-off-by: lstocchi <lstocchi@redhat.com>

pkg/machine/shim/networking.go
@@ -92,7 +92,7 @@ func startHostForwarder(mc *vmconfigs.MachineConfig, provider vmconfigs.VMProvid
 
 func startNetworking(mc *vmconfigs.MachineConfig, provider vmconfigs.VMProvider) (string, machine.APIForwardingState, error) {
 	// Check if SSH port is in use, and reassign if necessary
-	if !ports.IsLocalPortAvailable(mc.SSH.Port) {
+	if provider.UserModeNetworkEnabled(mc) && !ports.IsLocalPortAvailable(mc.SSH.Port) {
 		logrus.Warnf("detected port conflict on machine ssh port [%d], reassigning", mc.SSH.Port)
 		if err := reassignSSHPort(mc, provider); err != nil {
 			return "", 0, err
@@ -145,7 +145,11 @@ func conductVMReadinessCheck(mc *vmconfigs.MachineConfig, maxBackoffs int, backo
 			sshError = ErrNotRunning
 			continue
 		}
-		if !isListening(mc.SSH.Port) {
+		address := "localhost"
+		if mc.HyperVHypervisor.IPAddress != "" {
+			address = mc.HyperVHypervisor.IPAddress
+		}
+		if !isListening(address, mc.SSH.Port) {
 			sshError = ErrSSHNotListening
 			continue
 		}
@@ -158,7 +162,7 @@ func conductVMReadinessCheck(mc *vmconfigs.MachineConfig, maxBackoffs int, backo
 		// CoreOS users have reported the same observation but
 		// the underlying source of the issue remains unknown.
 
-		if sshError = machine.CommonSSHSilent(mc.SSH.RemoteUsername, mc.SSH.IdentityPath, mc.Name, mc.SSH.Port, []string{"true"}); sshError != nil {
+		if sshError = machine.CommonSSHSilentWithAddress(mc.SSH.RemoteUsername, mc.SSH.IdentityPath, mc.Name, address, mc.SSH.Port, []string{"true"}); sshError != nil {
 			logrus.Debugf("SSH readiness check for machine failed: %v", sshError)
 			continue
 		}
@@ -218,9 +222,9 @@ func reassignSSHPort(mc *vmconfigs.MachineConfig, provider vmconfigs.VMProvider)
 	return nil
 }
 
-func isListening(port int) bool {
+func isListening(address string, port int) bool {
 	// Check if we can dial it
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", "127.0.0.1", port), 10*time.Millisecond)
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", address, port), 10*time.Millisecond)
 	if err != nil {
 		return false
 	}

pkg/machine/ssh.go
@@ -17,28 +17,36 @@ import (
 // and a port
 // TODO This should probably be taught about an machineconfig to reduce input
 func CommonSSH(username, identityPath, name string, sshPort int, inputArgs []string) error {
-	return commonBuiltinSSH(username, identityPath, name, sshPort, inputArgs, true, os.Stdin)
+	return commonBuiltinSSH(username, identityPath, name, "localhost", sshPort, inputArgs, true, os.Stdin)
+}
+
+func CommonSSHShellWithAddress(username, identityPath, name, address string, sshPort int, inputArgs []string) error {
+	return commonNativeSSH(username, identityPath, name, address, sshPort, inputArgs, os.Stdin)
 }
 
 func CommonSSHShell(username, identityPath, name string, sshPort int, inputArgs []string) error {
-	return commonNativeSSH(username, identityPath, name, sshPort, inputArgs, os.Stdin)
+	return CommonSSHShellWithAddress(username, identityPath, name, "localhost", sshPort, inputArgs)
 }
 
 func CommonSSHSilent(username, identityPath, name string, sshPort int, inputArgs []string) error {
-	return commonBuiltinSSH(username, identityPath, name, sshPort, inputArgs, false, nil)
+	return commonBuiltinSSH(username, identityPath, name, "localhost", sshPort, inputArgs, false, nil)
 }
 
 func CommonSSHWithStdin(username, identityPath, name string, sshPort int, inputArgs []string, stdin io.Reader) error {
-	return commonBuiltinSSH(username, identityPath, name, sshPort, inputArgs, true, stdin)
+	return commonBuiltinSSH(username, identityPath, name, "localhost", sshPort, inputArgs, true, stdin)
 }
 
-func commonBuiltinSSH(username, identityPath, name string, sshPort int, inputArgs []string, passOutput bool, stdin io.Reader) error {
+func CommonSSHSilentWithAddress(username, identityPath, name, address string, sshPort int, inputArgs []string) error {
+	return commonBuiltinSSH(username, identityPath, name, address, sshPort, inputArgs, false, nil)
+}
+
+func commonBuiltinSSH(username, identityPath, name, address string, sshPort int, inputArgs []string, passOutput bool, stdin io.Reader) error {
 	config, err := createConfig(username, identityPath)
 	if err != nil {
 		return err
 	}
 
-	client, err := ssh.Dial("tcp", fmt.Sprintf("localhost:%d", sshPort), config)
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", address, sshPort), config)
 	if err != nil {
 		return err
 	}
@@ -109,8 +117,8 @@ func createConfig(user string, identityPath string) (*ssh.ClientConfig, error) {
 	}, nil
 }
 
-func commonNativeSSH(username, identityPath, name string, sshPort int, inputArgs []string, stdin io.Reader) error {
-	sshDestination := username + "@localhost"
+func commonNativeSSH(username, identityPath, name, address string, sshPort int, inputArgs []string, stdin io.Reader) error {
+	sshDestination := username + "@" + address
 	port := strconv.Itoa(sshPort)
 	interactive := true
 

cfergeau/podman-macadam

pkg/machine/shim/networking.go
@@ -92,7 +92,7 @@ func startHostForwarder(mc *vmconfigs.MachineConfig, provider vmconfigs.VMProvid
 
 func startNetworking(mc *vmconfigs.MachineConfig, provider vmconfigs.VMProvider) (string, machine.APIForwardingState, error) {
 	// Check if SSH port is in use, and reassign if necessary
-	if !ports.IsLocalPortAvailable(mc.SSH.Port) {
+	if provider.UserModeNetworkEnabled(mc) && !ports.IsLocalPortAvailable(mc.SSH.Port) {
 		logrus.Warnf("detected port conflict on machine ssh port [%d], reassigning", mc.SSH.Port)
 		if err := reassignSSHPort(mc, provider); err != nil {
 			return "", 0, err
@@ -145,7 +145,11 @@ func conductVMReadinessCheck(mc *vmconfigs.MachineConfig, maxBackoffs int, backo
 			sshError = ErrNotRunning
 			continue
 		}
-		if !isListening(mc.SSH.Port) {
+		address := "localhost"
+		if mc.HyperVHypervisor.IPAddress != "" {
+			address = mc.HyperVHypervisor.IPAddress
+		}
+		if !isListening(address, mc.SSH.Port) {
 			sshError = ErrSSHNotListening
 			continue
 		}
@@ -158,7 +162,7 @@ func conductVMReadinessCheck(mc *vmconfigs.MachineConfig, maxBackoffs int, backo
 		// CoreOS users have reported the same observation but
 		// the underlying source of the issue remains unknown.
 
-		if sshError = machine.LocalhostSSHSilent(mc.SSH.RemoteUsername, mc.SSH.IdentityPath, mc.Name, mc.SSH.Port, []string{"true"}); sshError != nil {
+		if sshError = machine.LocalhostSSHSilentWithAddress(mc.SSH.RemoteUsername, mc.SSH.IdentityPath, mc.Name, address, mc.SSH.Port, []string{"true"}); sshError != nil {
 			logrus.Debugf("SSH readiness check for machine failed: %v", sshError)
 			continue
 		}
@@ -218,9 +222,9 @@ func reassignSSHPort(mc *vmconfigs.MachineConfig, provider vmconfigs.VMProvider)
 	return nil
 }
 
-func isListening(port int) bool {
+func isListening(address string, port int) bool {
 	// Check if we can dial it
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", "127.0.0.1", port), 10*time.Millisecond)
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", address, port), 10*time.Millisecond)
 	if err != nil {
 		return false
 	}

pkg/machine/ssh.go
@@ -17,24 +17,29 @@ import (
 // and a port
 // TODO This should probably be taught about an machineconfig to reduce input
 func LocalhostSSH(username, identityPath, name string, sshPort int, inputArgs []string) error {
-	return localhostBuiltinSSH(username, identityPath, name, sshPort, inputArgs, true, os.Stdin)
+	return localhostBuiltinSSH(username, identityPath, name, "localhost", sshPort, inputArgs, true, os.Stdin)
+}
+
+// This ssh’es to a podman machine which is not listening on localhost but has its own IP
+func LocalhostSSHShellWithAddress(username, identityPath, name, address string, sshPort int, inputArgs []string) error {
+	return localhostNativeSSH(username, identityPath, name, address, sshPort, inputArgs, os.Stdin, false)
 }
 
 // LocalhostSSHShellForceTerm runs the native ssh shell client and forces a terminal (-t)
 func LocalhostSSHShellForceTerm(username, identityPath, name string, sshPort int, inputArgs []string) error {
-	return localhostNativeSSH(username, identityPath, name, sshPort, inputArgs, os.Stdin, true)
+	return localhostNativeSSH(username, identityPath, name, "localhost", sshPort, inputArgs, os.Stdin, true)
 }
 
 func LocalhostSSHShell(username, identityPath, name string, sshPort int, inputArgs []string) error {
-	return localhostNativeSSH(username, identityPath, name, sshPort, inputArgs, os.Stdin, false)
+	return localhostNativeSSH(username, identityPath, name, "localhost", sshPort, inputArgs, os.Stdin, false)
 }
 
 func LocalhostSSHSilent(username, identityPath, name string, sshPort int, inputArgs []string) error {
-	return localhostBuiltinSSH(username, identityPath, name, sshPort, inputArgs, false, nil)
+	return localhostBuiltinSSH(username, identityPath, name, "localhost", sshPort, inputArgs, false, nil)
 }
 
 func LocalhostSSHWithStdin(username, identityPath, name string, sshPort int, inputArgs []string, stdin io.Reader) error {
-	return localhostBuiltinSSH(username, identityPath, name, sshPort, inputArgs, true, stdin)
+	return localhostBuiltinSSH(username, identityPath, name, "localhost", sshPort, inputArgs, true, stdin)
 }
 
 // LocalhostSSHCopy uses scp to copy files from/to a localhost machine using ssh.
@@ -61,13 +66,17 @@ func LocalhostSSHCopy(username, identityPath string, sshPort int, srcPath, destP
 	return cmd.Run()
 }
 
-func localhostBuiltinSSH(username, identityPath, name string, sshPort int, inputArgs []string, passOutput bool, stdin io.Reader) error {
+func LocalhostSSHSilentWithAddress(username, identityPath, name, address string, sshPort int, inputArgs []string) error {
+	return localhostBuiltinSSH(username, identityPath, name, address, sshPort, inputArgs, false, nil)
+}
+
+func localhostBuiltinSSH(username, identityPath, name, address string, sshPort int, inputArgs []string, passOutput bool, stdin io.Reader) error {
 	config, err := createLocalhostConfig(username, identityPath) // WARNING: This MUST NOT be generalized to allow communication over untrusted networks.
 	if err != nil {
 		return err
 	}
 
-	client, err := ssh.Dial("tcp", fmt.Sprintf("localhost:%d", sshPort), config)
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", address, sshPort), config)
 	if err != nil {
 		return err
 	}
@@ -146,8 +155,8 @@ func createLocalhostConfig(user string, identityPath string) (*ssh.ClientConfig,
 	}, nil
 }
 
-func localhostNativeSSH(username, identityPath, name string, sshPort int, inputArgs []string, stdin io.Reader, forceTerm bool) error {
-	sshDestination := username + "@localhost"
+func localhostNativeSSH(username, identityPath, name, address string, sshPort int, inputArgs []string, stdin io.Reader, forceTerm bool) error {
+	sshDestination := username + "@" + address
 	port := strconv.Itoa(sshPort)
 	interactive := true
 